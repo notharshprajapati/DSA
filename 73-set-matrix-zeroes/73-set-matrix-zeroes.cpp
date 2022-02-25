2e6b94a523f689bf90f76d1fe1816ba8ca2af957
Harsh <59908775+notharshprajapati@users.noreply.github.com>
2022-02-25 16:23:49 +0530

    Time: 12 ms (91.86%), Space: 13.1 MB (95.75%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,47 +1,34 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) 
-    { 
-       
-        int m = matrix.size();
-        int n = matrix[0].size();
+    { //revised
+        int rowsize = matrix.size();
+        int columnsize = matrix[0].size();
+
+       vector<int>rowmarker(rowsize,1);
+       vector<int>columnmarker(columnsize,1);
         
-        int arr[m][n];
-         for(int i=0 ; i<m;i++)
+        for(int i=0 ; i<rowsize;i++)
         {
-           for(int j=0 ; j<n;j++) 
-           {
-                arr[i][j]=1;
-           }
-        } 
-      
-        
-        
-        for(int i=0 ; i<m;i++)
-        {
-           for(int j=0 ; j<n;j++) 
+           for(int j=0 ; j<columnsize;j++) 
            {
                if(matrix[i][j]==0)
-                   arr[i][j]=0;
+               {
+                   rowmarker[i]=0;
+                columnmarker[j]=0;
+               }
+                   
            }
         }  
         
         
-        
-         for(int i=0 ; i<m;i++)
+          for(int i=0 ; i<rowsize;i++)
         {
-           for(int j=0 ; j<n;j++) 
+           for(int j=0 ; j<columnsize;j++) 
            {
-               if(arr[i][j]==0)
+               if(rowmarker[i]==0 ||   columnmarker[j]==0)
                {
-                   for(int a=0 ; a<m;a++)
-                    {
-                        matrix[a][j]=0;
-                    }
-                   for(int b=0 ; b<n;b++) 
-                    {
-                        matrix[i][b]=0;
-                    }
+                   matrix[i][j]=0;
                }
            }
         }  
