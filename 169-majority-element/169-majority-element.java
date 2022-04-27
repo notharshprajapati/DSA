8a4e17e582cf45a5dba0acd1ff2a078113ee0da6
Harsh <59908775+notharshprajapati@users.noreply.github.com>
2022-04-27 17:58:37 +0530

    Time: 3 ms (59.10%), Space: 56.1 MB (46.61%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,22 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int size = nums.length;
-        for(int i =0;i<size;i++)
-        {
-            int pointer = nums[i];
-            int count = 0;
-            for(int j = i;j<size;j++)
-            {
-                if(pointer == nums[j])
-                {
-                    count++;
-                }
-            }
-            if(count > size/2)
-            {
-                return pointer;
-            }
-        }
-        return 0;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
