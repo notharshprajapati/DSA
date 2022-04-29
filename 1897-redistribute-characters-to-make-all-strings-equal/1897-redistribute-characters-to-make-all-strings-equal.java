57f74c6d677de8d8e9410b6f154d7ad01f7700ab
Harsh <59908775+notharshprajapati@users.noreply.github.com>
2022-04-29 13:06:06 +0530

    Time: 4 ms (62.03%), Space: 43.4 MB (59.40%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean makeEqual(String[] s) {
         int arr[] = new int[26];
-        int size = s.length;
-        for(int k =0;k<size;k++)
+        for(int k =0;k<s.length;k++)
         {
             for (int i = 0; i < s[k].length(); i++) {
                 arr[(int)s[k].charAt(i)-97]++;
@@ -10,7 +9,7 @@ class Solution {
         }
         for(int i =0 ; i<26;i++)
         {
-            if(arr[i]%size != 0)
+            if(arr[i]%s.length != 0)
             {
                 return false;
             }
