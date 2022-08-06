9f17b5cecdcec212c84df1428fd35186e9fa04a2
Harsh <59908775+notharshprajapati@users.noreply.github.com>
2022-08-06 13:07:35 +0530

    Time: 1 ms (89.32%), Space: 42.1 MB (80.26%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,5 +1,29 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+    long nn = n;
+    if (n < 0) nn = -n;
+    double ans = power(x, nn);
+    if (n < 0) ans = 1 / ans;
+    return ans;
     }
+    
+    public double power(double x, long n) {
+    if (n == 0) {
+      return 1;
+    }
+    if (n == 1) {
+      return x;
+    }
+
+    double ans = 0;
+    if (n % 2 == 0) {
+      ans = power(x, n / 2);
+      ans = ans * ans;
+    } else {
+      ans = power(x, n / 2);
+      ans = ans * ans * x;
+    }
+
+    return ans;
+  }
 }
