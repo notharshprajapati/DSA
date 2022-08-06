8398759546882956b748fbd3673c86087e300f62
Harsh <59908775+notharshprajapati@users.noreply.github.com>
2022-08-06 11:11:55 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (68.85%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,6 +1,6 @@
 class Solution {
-    public boolean searchMatrix(int[][] arr, int tar) {
-        
+
+  public boolean searchMatrix(int[][] arr, int tar) {
     int n = arr.length;
     int m = arr[0].length;
 
@@ -8,15 +8,11 @@ class Solution {
     int j = m - 1;
 
     while (i < n && j >= 0) {
-      System.out.println(arr[i][j] + " " + i + " " + j);
       if (arr[i][j] == tar) {
         return true;
-      } else if (arr[i][j] < tar) {
-        i++;
-      } else if (arr[i][j] > tar) {
-        j--;
       }
+      if (arr[i][j] < tar) i++; else j--;
     }
     return false;
-    }
+  }
 }
